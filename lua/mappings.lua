a778d8f65b5333243971f175224306dce4d51fb9
Darvesh Gorhe <darveshgorhe@gmail.com>
2025-10-27 14:46:47 -0400

    Added mapping for triggering file search in Telescope via ripgrep

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,4 +13,9 @@ map("n", "<leader>ft", function()
   require("telescope.builtin").live_grep()
 end, { desc = "Telescope Live Grep (Find Text)" })
 
+-- File search using ripgrep
+map("n", "<leader>fs", function()
+  require("telescope.builtin").grep_string()
+end, { desc = "Telescope Grep String (Find in Files)" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
